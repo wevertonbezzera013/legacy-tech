3efe6cb10b26d7708cff3b293b9583af019951fc
Weverton <wevertonbezzera013@gmail.com>
2023-10-02 19:24:36 -0300

    fix: small tweak

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -69,7 +69,7 @@ export default function About() {
           </motion.div>
           <motion.div
             initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 2 }}
           >
